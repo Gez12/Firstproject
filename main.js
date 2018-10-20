5118e249d507a296cf27a2e5834981e28ce1fc11
Gez12 <gezahegnkea@gmail.com>
2018-10-20 12:45:59 -0500

    added pop up windows and console log

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
 let age;
-consol.log("age is currently: " + age);
+
+console.log("age is currently: " + age);
+
 age = 35;
-consol.log("age is currently: " + age);
+
+console.log("age is currently: " + age);
+
 age = 35 + 1; 
+
+console.log("hello world!");
+
+console.log("Today is final class day");
+
+console.log("I like the weather");
+
+let itRainsInDc = true;
+if (itRainsInDc) {
+    console.log("pack a rain coat");
+
+}
+else {
+    console.log("pack a sunshine glass");
+}
+
+let userName = prompt("what is your name");
+let nameConfirm = confirm("just checking, is " + userName + " your real name?");
+
+if(nameConfirm) {
+    alert("welcome to my page, " + userName + "!");
+}
+
+else {
+    userName = prompt("okay, what is your name?");
+}
+
+console.log("The user's name is " + userName + ".")
+
+
+
+
+
+ 
+
+ 
